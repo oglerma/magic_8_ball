1803b4147ae5d5eb8062518d0ad0718b5006c2d2
O gee Lerma <oglerma@gmail.com>
2018-04-02 12:28:24 -0700

    Added Pictures for  8 Ball

---
+++ magic_8_ball/magic_8_ball/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  magic_8_ball
+//
+//  Created by Ociel Lerma on 4/2/18.
+//  Copyright © 2018 Ociel Lerma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
